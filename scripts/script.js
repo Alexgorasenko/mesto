24b407d2c8e3b956cf325d92cc4455d69f595a3f
Alexandr Gorasenko <alex.gorasenko1605@gmail.comgit config --global user.name Alexandr>
2023-02-01 23:24:38 +0200

    сдача проекта 3 спринт

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+let aboutButton= document.querySelector(".profile__button_type_edit")
+let popup = document.querySelector(".popup")
+let closeBotton= popup.querySelector(".popup__close")
+let savebutton = document.querySelector('.popup__save');
+let nameForm = document.querySelector('.popup__form_type_name');
+let vocationForm = document.querySelector('.popup__form_type_vocation');
+let profileName = document.querySelector('.profile__name');
+let profileVocation = document.querySelector('.profile__vocation');
+let formElement= document.querySelector('.popup__eddit-form');
+
+function handleAboutButtonClick (){
+  popup.classList.add('popup__opened');
+  nameForm.value = profileName.textContent;
+  vocationForm.value = profileVocation.textContent;
+}
+
+function handleCloseButtonClick (){
+  popup.classList.remove('popup__opened');
+}
+
+aboutButton.addEventListener('click', handleAboutButtonClick);
+closeBotton.addEventListener('click', handleCloseButtonClick)
+
+function addInfo() {
+  profileName.textContent = nameForm.value;
+  profileVocation.textContent = vocationForm.value ;
+}
+
+savebutton.addEventListener('click', addInfo);
+savebutton.addEventListener('click', handleCloseButtonClick);
+formElement.addEventListener('submit', addInfo);
+
