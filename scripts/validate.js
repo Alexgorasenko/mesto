8fea05a5f43a791e2ea991a42729c4ff40978f2a
DESKTOP-89I0QNC\Администратор <alex.gorasenko1605@gmail.com>
2023-03-01 19:24:30 +0200

    Реализация валидации полей(появление ошибок)

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,26 +1,54 @@
-const formValidationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
+const formValidationCinfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+  submitButtonSelector: ".popup__save",
+};
+
+
+
+const showInputError = (formElement,inputElement, errorMessage,config) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+};
+
+
+const hideInputError = (formElement,inputElement,config) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = '';
+};
+
+
+const isValid = (formElement, inputElement,config) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement,inputElement, inputElement.validationMessage, config);
+  } else {
+    hideInputError(formElement,inputElement, config);
+  }
+};
+
+const setEventListeners = (formElement, config)=>{
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector))
+  inputList.forEach((inputElement)=>{
+    inputElement.addEventListener('input',()=>{
+      isValid(formElement,inputElement,config)
+    })
+  })
 }
 
-const disableSubmit = (event) =>{
-  event.preventDefault();
+const enableValidation = (config)=>{
+  const formList = Array.from(document.querySelectorAll(config.formSelector))
+  formList.forEach((formElement)=>{
+    setEventListeners (formElement, config)
+    formElement.addEventListener('submit',(event)=>{
+      event.preventDefault();
+    })
+  })
 }
 
-
-const addInputListners = (form, config) =>{
-  const inputList = Array.from(form.querySelector(config.inputSelector));
-  inputList.forEach((item) => {
-    console.log(item)
-  });
-}
-
-const enableValidation = (config) =>{
-  const form = Array.from(document.querySelector(config.formSelector))
-  addInputListners(form,config)
-  form.addEventListener('submit', disableSubmit)
-  console.log(form)
-}
-
-
-enableValidation (formValidationConfig)
+enableValidation (formValidationCinfig)
